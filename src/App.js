6463a2cededc726559e9bf63a560613a4deea228
Dennis <hammarstrand@hotmail.com>
2019-05-28 15:22:34 +0200

    Updated readme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./index.css";
-// import Nav from "./components/Nav/Nav";
 import Todo from "./components/Todo/Todo";
 
 function App() {
@@ -8,14 +7,13 @@ function App() {
     <>
       <header className="header">
         <h1>todos</h1>
-        {/* <Nav /> */}
       </header>
       <Todo />
       <footer>
         <p>Double-click to edit a todo</p>
         <p>
           Created by Dennis Hammarstrand{" "}
-          <a href="https://github.com/dennishammarstrand">
+          <a href="https://github.com/dennishammarstrand/ReactTodo">
             <i className="fab fa-github" />
           </a>
         </p>
